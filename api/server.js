1eae32562a1c57bdea236580a79bc73576872726
Ben <bbjberg91@gmail.com>
2020-04-13 17:29:15 -0400

    MVP

--- api/server.js
+++ api/server.js
@@ -31,5 +31,48 @@ server.get('/api/accounts/:id', (req, res) =>{
     })
 })
 
+server.post('/api/accounts', (req, res) =>{
+    const accData= req.body;
+    db('accounts').insert(accData, 'id')
+    .then(ids => {
+        const id = ids[0];
+        db('accounts').where({ id }).first()
+        .then(accounts => {
+            res.status(200).json({ data: accounts})
+        })
+    }).catch(error =>{
+        res.status(500).json({message: 'an error has occurred'})
+    })
+})
+
+server.patch('/api/accounts/:id', (req, res)=>{
+    const changes= req.body;
+    const {id} = req.params;
+    db('accounts').where({ id })
+    .update(changes)
+    .then(count => {
+        if (count > 0 ){
+            res.status(200).json({ message: 'account updated'})
+        } else {
+            res.status(404).json({message: 'no account with that ID found'})
+        }
+    }).catch(error =>{
+        res.status(500).json({ messsage: 'an error has occurred'})
+    })
+})
+
+server.delete('/api/accounts/:id', (req, res) =>{
+    const {id} = req.params;
+    db('accounts').where({ id })
+    .del().then(el =>{
+        if (el > 0){
+            res.status(202).json({message: 'account deleted'})
+        } else {
+            res.status(404).json({ message: 'no account matches that ID'})
+        }
+    }).catch(error =>{
+        res.status(500).json({message: 'an error has occurred'})
+    })
+})
 
 module.exports = server;
